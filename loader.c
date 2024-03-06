03071ae797849e12cca4e4020bf330c37eb5d7e3
olgaoznovich <ol.oznovich@gmail.com>
2024-03-06 12:58:48 +0200

    i think i finished lab 5

--- loader.c
+++ loader.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+extern int startup(int argc, char **argv, void (*start)());
+
 // task0
 //  void print_phdr(Elf32_Phdr *phdr, int i) {
 //      printf("Program header number %d at address %p\n", i, phdr);
@@ -37,20 +39,45 @@ int foreach_phdr(void *map_start, void (*func)(Elf32_Phdr *, int), int arg)
     {
         if (phdr[i].p_type == PT_LOAD) // Only load LOAD segments
         {
-            func(&phdr[i], i);
+            func(&phdr[i], arg);
         }
     }
     return 0;
 }
 
+void load_phdr(Elf32_Phdr *phdr, int fd)
+{
+    if (phdr->p_type == PT_LOAD) {
+        void* segment_address = mmap(
+            (void*)(phdr->p_vaddr & 0xfffff000),           
+            phdr->p_memsz + (phdr->p_vaddr & 0xfff),
+            (phdr->p_flags & PF_X ? PROT_EXEC : 0) |
+            (phdr->p_flags & PF_W ? PROT_WRITE : 0) |
+            (phdr->p_flags & PF_R ? PROT_READ : 0),
+            MAP_PRIVATE | MAP_FIXED,  
+            fd,                 
+            (phdr->p_offset & 0xfffff000)
+        );
+
+        if (segment_address == MAP_FAILED) {
+            printf("error in mapping to address %p\n", (void*)phdr->p_vaddr);
+        } else {
+            print_phdr(phdr, 0);
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
-    int fd = open(argv[1], O_RDONLY);
+    char* program = argv[1];
+    int fd = open(program, O_RDONLY);
     struct stat st;
     fstat(fd, &st);
     size_t size = st.st_size;
     void *map_start = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
-    foreach_phdr(map_start, print_phdr, 0);
+    // foreach_phdr(map_start, print_phdr, 0);
+    foreach_phdr(map_start, load_phdr, fd);
+    startup(argc - 2, argv + 2, (void *)(((Elf32_Ehdr *)map_start)->e_entry));
     munmap(map_start, size);
     close(fd);
     return 0;
--- makefile
+++ makefile
@@ -1,10 +1,13 @@
 all: loader
 
-loader: loader.o
-	gcc -m32 -Wall -g -o loader loader.o
+loader: loader.o start.o
+	ld -o loader loader.o start.o startup.o -L/usr/lib32 -lc -T linking_script -dynamic-linker /lib32/ld-linux.so.2
 
 loader.o: loader.c
 	gcc -m32 -Wall -g -c -o loader.o loader.c
+
+start.o: start.s
+	nasm -f elf32 start.s -o start.o
 
 .PHONY: clean
 
